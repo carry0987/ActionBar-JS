627d76daa237874bc632a3846d942bf79d1db545
carry0987 <carry0987@users.noreply.github.com>
2023-10-22 22:44:55 +0800

    Fix stylesheet injection

--- src/util.js
+++ src/util.js
@@ -37,7 +37,7 @@ const Util = {
         style.appendChild(document.createTextNode(''));
         document.head.appendChild(style);
 
-        let stylesheet = document.styleSheets[document.styleSheets.length - 1];
+        let stylesheet = style.sheet;
 
         for (let selector in stylesObject) {
             if (stylesObject.hasOwnProperty(selector)) {
