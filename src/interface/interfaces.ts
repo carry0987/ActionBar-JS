8de49f2060866f124dbb506f12bf350630554976
carry0987 <carry0987@users.noreply.github.com>
2024-02-07 16:09:28 +0800

    Refactor with TypeScript

---
+++ src/interface/interfaces.ts
@@ -0,0 +1,42 @@
+export interface OnLoadCallback {
+    (actionBar: any): void;
+}
+
+export interface OnActionCallback {
+    (button?: HTMLButtonElement): void;
+}
+
+export interface ActionBarOption {
+    countMsg?: string;
+    countPlaceholder?: string;
+    hideButton?: string[];
+    template?: string | Element;
+    styles?: object;
+    customButton?: Array<{ name: string; icon: string; callback: Function }>;
+    onLoad?: OnLoadCallback;
+    onClear?: OnActionCallback;
+    onSelectAll?: OnActionCallback;
+    onRestore?: OnActionCallback;
+    onMove?: OnActionCallback;
+    onDelete?: OnActionCallback;
+}
+
+export interface ActionBarCallback {
+    onClear?: OnActionCallback;
+    onSelectAll?: OnActionCallback;
+    onRestore?: OnActionCallback;
+    onMove?: OnActionCallback;
+    onDelete?: OnActionCallback;
+}
+
+export interface ActionBarButtons {
+    [key: string]: HTMLButtonElement | null
+}
+
+export enum Action {
+    CLEAR = 'clear',
+    SELECT_ALL = 'selectAll',
+    RESTORE = 'restore',
+    MOVE = 'move',
+    DELETE = 'delete'
+}
---
+++ src/module/config.ts
@@ -0,0 +1,16 @@
+import { ActionBarOption } from '../interface/interfaces';
+
+export const defaults: ActionBarOption = {
+    countMsg: 'Selected (:num) items',
+    countPlaceholder: '(:num)',
+    hideButton: [],
+    template: undefined,
+    styles: {},
+    customButton: [],
+    onLoad: undefined,
+    onClear: undefined,
+    onSelectAll: undefined,
+    onRestore: undefined,
+    onMove: undefined,
+    onDelete: undefined
+};
